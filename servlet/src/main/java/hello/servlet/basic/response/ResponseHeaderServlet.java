dc1afe858aa1bd5c94e6b5f73487340d2cb13264
somemellow <somemellow74@gmail.com>
2022-11-07 17:15:31 +0900

    스프링MVC 1 - 백엔 웹 개발 핵심기술: HttpResponseServlet 기본 사용법 편의기능 쿠키 기능

--- servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,7 +24,26 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.setHeader("Pragma", "no-cache");
         response.setHeader("my-header", "hello");
 
+        //[Header 편의 메서드]
+        content(response);
+        cookie(response);
+
         PrintWriter writer=response.getWriter();
         writer.print("ok");
      }
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2); //(생략시 자동 생성)
+    }
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        // response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
 }
