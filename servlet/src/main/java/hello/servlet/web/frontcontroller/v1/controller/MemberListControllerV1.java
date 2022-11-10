6fe9bb3ed342eea16f7ab176a636d0efd5304d20
somemellow <somemellow74@gmail.com>
2022-11-10 17:42:22 +0900

    스프링MVC 1 - 백엔 웹 개발 핵심기술서블릿 MVC 프레임 워크 만들기 MemberListControllerV1 추가

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -9,23 +9,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-public class MemberSaveControllerV1 implements ControllerV1 {
+public class MemberListControllerV1 implements ControllerV1 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
 
-        Member member = new Member(username, age);
-        memberRepository.save(member);
-
-        request.setAttribute("member", member);
-
-        String viewPath = "/WEB-INF/views/save-result.jsp";
+        String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-
+        dispatcher.forward(request, response);
     }
 }
