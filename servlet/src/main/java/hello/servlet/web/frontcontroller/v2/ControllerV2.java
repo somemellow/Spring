5f35191193f1cf9d4cc8138504351b09fd665a80
somemellow <somemellow74@gmail.com>
2022-11-11 16:42:46 +0900

    스프링MVC 1 - 백엔 웹 개발 핵심기술서블릿 MVC 프레임 워크 만들기v2 Controller 생성

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
@@ -1,4 +1,12 @@
 package hello.servlet.web.frontcontroller.v2;
 
+import hello.servlet.web.frontcontroller.MyView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+
 public interface ControllerV2 {
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws SerialException, IOException;
 }
