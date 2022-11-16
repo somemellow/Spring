6f8f6ef95067fff875178e14851d04d27c6107ee
somemellow <somemellow74@gmail.com>
2022-11-16 17:25:55 +0900

    스프링MVC 1 - 백엔 웹 개발 핵심기술서블릿 MVC 프레임 워크 만들기v4 WebServlet name url patterns 추가 v5 FrontControllerV5 추가

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -6,6 +6,7 @@ import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
 import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
 public class FrontControllerServletV4 extends HttpServlet {
     private Map<String,ControllerV4> controllerMap = new HashMap<>();
 
