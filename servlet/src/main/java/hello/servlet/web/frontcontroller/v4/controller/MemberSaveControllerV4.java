966a844b30246f165807fbe608138385bd6faa5b
somemellow <somemellow74@gmail.com>
2022-11-21 17:56:46 +0900

    OldController Class 추가

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -13,7 +13,7 @@ public class MemberSaveControllerV4 implements ControllerV4 {
     @Override
     public String process(Map<String, String> paramMap, Map<String, Object> model) {
         String username = paramMap.get("username");
-        int age = Integer.getInteger(paramMap.get("age"));
+        int age = Integer.parseInt(paramMap.get("age"));
 
         Member member = new Member(username,age);
         memberRepository.save(member);
